fc6b82614ae030b609be9428ba0ce7cac7139342
dg@cowlark.com <dg>
2012-07-06 22:28:41 +0000

    Method declarations.

--- compiler/src/com/cowlark/cowjac/Translator.scala
+++ compiler/src/com/cowlark/cowjac/Translator.scala
@@ -18,6 +18,8 @@ import soot.ShortType
 import soot.ByteType
 import soot.RefLikeType
 import soot.RefType
+import soot.SootMethod
+import soot.ArrayType
 
 object Translator
 {
@@ -64,6 +66,13 @@ object Translator
 			override def caseFloatType(t: FloatType) = ps.print("jfloat")
 			override def caseDoubleType(t: DoubleType) = ps.print("jdouble")
 			
+			override def caseArrayType(t: ArrayType)
+			{
+				ps.print("com::cowlark::cowjac::Array<")
+				t.getElementType.apply(TS)
+				ps.print(">*")
+			}
+			
 			override def caseRefType(t: RefType)
 			{
 				ps.print(javaToCXX(t.getSootClass.getName))
@@ -79,6 +88,7 @@ object Translator
 	{
 		val isref = field.getType.isInstanceOf[RefLikeType]
 
+		hps.print("\t")
 		translateModifier(field, hps)
 		if (isref)
 			hps.print("com::cowlark::cowjac::GlobalReference<")
@@ -108,6 +118,31 @@ object Translator
 			cps.print(field.getName)
 			cps.print(";\n")
 		}
+	}
+	
+	def translateMethodDeclaration(method: SootMethod, hps: PrintStream)
+	{
+		hps.print("\t")
+		translateModifier(method, hps)
+		
+		translateType(method.getReturnType, hps)
+		hps.print(" ")
+		hps.print(method.getName)
+		hps.print("(com::cowlark::cowjac::Context*")
+		
+		for (to <- method.getParameterTypes)
+		{
+			val t = to.asInstanceOf[Type]
+
+			hps.print(", ")
+			translateType(t, hps)
+		}
+			
+		hps.print(")")
+		
+		if (method.isAbstract)
+			hps.print(" = 0")
+		hps.print(";\n")
 	}
 	
 	def translate(sootclass: SootClass, cps: PrintStream, hps: PrintStream)
@@ -140,12 +175,23 @@ object Translator
 		
 		hps.print("\n{\n")
 		
+		hps.print("\t/* Field declarations */\n")
+		cps.print("/* Field definitions */\n")
 		for (f <- sootclass.getFields)
 		{
 			translateFieldDeclaration(f, hps)
 			translateFieldDefinition(f, cps)
 		}
-			
+		
+		hps.print("\n")
+		
+		hps.print("\t/* Method declarations */\n")
+		cps.print("/* Method definitions */\n")
+		for (m <- sootclass.getMethods)
+		{
+			translateMethodDeclaration(m, hps)
+		}
+		
 		hps.print("};\n")
 		hps.print("\n")
 		
--- library/src/test/Main.java
+++ library/src/test/Main.java
@@ -1,12 +1,19 @@
 package test;
 
-public class Main
+public abstract class Main
 {
 	private int i_private;
 	public long i_public;
 	protected double i_protected;
 	char i_default;
 	static Main i_static;
+	
+	private void m_private() {}
+	public void m_public() {}
+	protected void m_protected() {}
+	void m_default() {}
+	static void m_static() {}
+	public abstract void m_abstract();
 	
 	public static final void main(String[] argv)
 	{
